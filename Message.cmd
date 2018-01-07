c39ed770758f3d58a3127b77835dfb70ab4c15ed
DESKTOP-8M0J74Q\User <nwy140@gmail.com>
2018-01-07 22:16:23 +0800

    no message

---
+++ Message.cmd
@@ -0,0 +1,17 @@
+
+
+start powershell ".\abc.ps1" -windowstyle hidden
+
+msg * Message from previous user of this computer
+msg * This is your current computer's specification
+dxdiag
+msg * Notice the yellowness of your screen, it is flux
+
+msg * flux adjust bluelight filter and brightness to reduce eye strain
+msg * keep it or uninstall it, its up to you
+
+msg * Chia's folder is at my documents
+
+msg * Java sdk is installed in this computer
+
+msg * visit me at https://bit.ly/nwymof
